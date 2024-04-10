e0254744f60394e8b254dc5cad7bf138ff040a63
Fida Hussain <hyderfida14@gmail.com>
2024-04-10 22:37:41 +0530

    error handling

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,31 @@
-const  MealsPage = () => {
+import Link from 'next/link';
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+ export default async function  MealsPage ()  {
+    const meals  = await getMeals(); 
+
     return (
-        <h1>Meals page</h1>
+       <>
+         <header className={classes.header}>
+            <h1>
+            Delicious meals, created
+            <span className={classes.highlight}> by you</span>
+            </h1>
+            <p>
+                Choose your favorite recipe and cook it yousrself. Its easy and fun!
+            </p>
+            <p className={classes.cta}>
+                <Link href='/meals/share'>
+                    Share Your Favorite Recipe
+                </Link>
+            </p>
+          </header>
+         <main className={classes.main}>
+            <MealsGrid meals = {meals}/>
+         </main>
+       </>
     );
 };
 
-export default MealsPage;
--- components/nav-link.js
+++ components/nav-link.js
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import classes from './nav-link.module.css'
+import classes from "./nav-link.module.css";
 
-export default function NavLink({href, children}) {
+export default function NavLink({ href, children }) {
   const path = usePathname();
 
   return (
     <Link
       href={href}
-      className={path.startsWith(href) ? classes.active : undefined}
+      className={path.startsWith(href) ? `${classes.link} ${classes.active} ` : classes.link}
     >
       {children}
     </Link>
